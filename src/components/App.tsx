d270704ab4c97495d4bfdd07bfe0fe53cd55708b
F34th3R <jafetlopezch@gmail.com>
2020-09-22 09:47:29 -0600

    Navigation all done! 💃🏻
     On branch master
     Changes to be committed:
    	modified:   src/components/App.tsx
    	modified:   src/components/utils/Navigation/Navigation.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { useRecoilValue } from 'recoil'
 import { Header, CustomCursor, Navigation } from './utils'
 import { HomeBanner } from './pages/HomePage'
-import { toggleMenuState } from '../states'
 
 export const App = () => {
-  const toggleMenu = useRecoilValue(toggleMenuState)
   return (
     <>
       <CustomCursor />
       <Header />
-      {toggleMenu && <Navigation />}
+      <Navigation />
       <Switch>
         <Route path="/">
           <HomeBanner />
--- src/components/utils/Navigation/Navigation.tsx
+++ src/components/utils/Navigation/Navigation.tsx
@@ -5,8 +5,11 @@ import { Container, Flex, HambuergerButton } from '../../shared'
 import { Nav, NavHeader, NavFooter, NavVideos } from './NavigationStyles'
 // Constants
 import { NavigationList } from './NavigationList'
+import { toggleMenuState } from '../../../states'
+import { useRecoilValue } from 'recoil'
 
 export const Navigation: React.FC = () => {
+  const toggleMenu = useRecoilValue(toggleMenuState)
   const [revealVideo, setRevealVideo] = useState({
     show: false,
     video: 'featured-video.mp4',
@@ -16,40 +19,47 @@ export const Navigation: React.FC = () => {
   return (
     <>
       <AnimatePresence>
-        <Nav>
-          <Container>
-            <NavHeader>
-              <Flex spaceBetween>
-                <h2>Projects</h2>
-                <HambuergerButton />
-              </Flex>
-            </NavHeader>
-            <NavigationList setRevealVideo={setRevealVideo} />
-            <NavFooter></NavFooter>
-            <NavVideos>
-              <motion.div
-                className="reveal"
-                animate={{ width: revealVideo.show ? 0 : '100%' }}
-              ></motion.div>
-              <div className="video">
-                <AnimatePresence initial={false} exitBeforeEnter>
-                  <motion.video
-                    key={revealVideo.key}
-                    src={require(`../../../assets/video/${revealVideo.video}`)}
-                    initial={{ opacity: 0 }}
-                    exit={{ opacity: 0 }}
-                    animate={{
-                      opacity: 1
-                    }}
-                    transition={{ duration: 0.2, ease: 'easeInOut' }}
-                    loop
-                    autoPlay
-                  ></motion.video>
-                </AnimatePresence>
-              </div>
-            </NavVideos>
-          </Container>
-        </Nav>
+        {toggleMenu && (
+          <Nav
+            initial={{ x: '-100%' }}
+            exit={{ x: '-100%' }}
+            animate={{ x: toggleMenu ? 0 : '-100%' }}
+            transition={{ duration: 0.8, ease: [0.6, 0.05, -0.01, 0.9] }}
+          >
+            <Container>
+              <NavHeader>
+                <Flex spaceBetween>
+                  <h2>Projects</h2>
+                  <HambuergerButton />
+                </Flex>
+              </NavHeader>
+              <NavigationList setRevealVideo={setRevealVideo} />
+              <NavFooter></NavFooter>
+              <NavVideos>
+                <motion.div
+                  className="reveal"
+                  animate={{ width: revealVideo.show ? 0 : '100%' }}
+                ></motion.div>
+                <div className="video">
+                  <AnimatePresence initial={false} exitBeforeEnter>
+                    <motion.video
+                      key={revealVideo.key}
+                      src={require(`../../../assets/video/${revealVideo.video}`)}
+                      initial={{ opacity: 0 }}
+                      exit={{ opacity: 0 }}
+                      animate={{
+                        opacity: 1
+                      }}
+                      transition={{ duration: 0.2, ease: 'easeInOut' }}
+                      loop
+                      autoPlay
+                    ></motion.video>
+                  </AnimatePresence>
+                </div>
+              </NavVideos>
+            </Container>
+          </Nav>
+        )}
       </AnimatePresence>
     </>
   )
